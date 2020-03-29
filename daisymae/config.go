5403535a8bbe3dbc9f3b3feb60d4a847bde7dc8e
Yiping Su <ypsu@ucdavis.edu>
2020-03-28 20:19:35 -0700

    First Commit

---
+++ daisymae/config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// BotConfig represents bot configurations
+type BotConfig struct {
+	BotKey string `json:"botKey"`
+}
+
+// LoadConfig loads bot configuration variables
+func LoadConfig() (BotConfig, error) {
+	f, err := os.Open(".config")
+	if err != nil {
+		return BotConfig{}, err
+	}
+	var botConfig BotConfig
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&botConfig)
+	if err != nil {
+		return BotConfig{}, err
+	}
+	fmt.Println("successfully loaded .config")
+	return botConfig, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yiping-allison/daisymae
+
+go 1.14
+
+require github.com/bwmarrin/discordgo v0.20.2
